bd14080d563264b7cb6c7f36ee949151f1e65083
bengfarrell <ben@benfarrell.com>
2019-11-05 21:34:08 -0800

    bad file path through camel case

--- docs/babylon-scene/babylonscene.js
+++ docs/babylon-scene/babylonscene.js
@@ -1,5 +1,5 @@
 import BabylonScene from './src/babylonscene.js';
-import BaseApplication from './src/BaseApplication.js';
+import BaseApplication from './src/baseapplication.js';
 import DefaultStage from './src/defaultstage.js';
 import * as AddOns from './src/addons.js';
 
