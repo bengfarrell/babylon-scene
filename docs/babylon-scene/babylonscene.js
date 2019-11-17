539788a6825bf8da71fa3ecf210f32e14b40909a
bengfarrell <ben@benfarrell.com>
2019-11-17 14:54:53 -0800

    .idea ignore

--- docs/babylon-scene/babylonscene.js
+++
@@ -1,8 +0,0 @@
-import BabylonScene from './src/babylonscene.js';
-import BaseApplication from './src/baseapplication.js';
-import DefaultStage from './src/defaultstage.js';
-import * as AddOns from './src/addons.js';
-
-export {BabylonScene, BaseApplication, DefaultStage, AddOns};
-
-
